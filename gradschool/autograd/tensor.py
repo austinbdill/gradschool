425f1d6207a1d302726fb90e716221344537607b
Austin Dill <abdill@austins-mbp-2.myfiosgateway.com>
2025-10-21 22:04:27 -0400

    Add the simplest addition implementation

--- gradschool/autograd/tensor.py
+++ gradschool/autograd/tensor.py
@@ -1,9 +1,47 @@
-from typing import List
+from typing import List, Union
+from functools import reduce
+import operator
 
 
 class Tensor:
-    def __init__(self, data: List[float]) -> None:
+    """Initial tensor implementation"""
+
+    def __init__(
+        self, data: Union[float, List[float]], shape: Union[int, Tuple[int, ...]]
+    ) -> None:
+        """
+        :param data:
+        :param shape:
+        """
+        num_expected_elements = (
+            shape if isinstance(shape, int) else reduce(operator.mul, shape)
+        )
+        num_elements = 1 if isinstance(data, float) else len(data)
+        if num_expected_elements != num_elements:
+            raise ValueError(f"Invalid shape {shape} for data with length {len(data)}.")
+
         self._data = data
+        self._shape = shape
+        self._length = num_elements
+        self._ndim = 1 if isinstance(shape, int) else len(shape)
 
     def __add__(self, other: Tensor) -> Tensor:
-        pass
+        """
+        Sum data from self and other. 
+        :param other: 
+        :returns: 
+        """
+        # Need to have a more general solution for broadcasting. 
+        if self._shape != other._shape:
+            raise ValueError(f"Can't sum tensor with shape {self._shape} and tensor with shape {other._shape}")
+        summed_data = self._data + other._data if self._length == 1 else [self._data[i] + other._data[i] for i in range(self._length)]
+        return Tensor(summed_data, self._shape)
+
+    # TODO: Needs to be tested. 
+    def __eq__(self, other: Tensor) -> bool:
+        """
+        Check equality of two tensors. 
+        :param other:
+        :returns:
+        """ 
+        return self._shape == other._shape and self._data == other._data
--- gradschool/tests/autograd/test_tensor.py
+++ gradschool/tests/autograd/test_tensor.py
@@ -3,6 +3,47 @@ import pytest
 from gradschool.autograd.tensor import Tensor
 
 
-def test_sanity():
-    tensor = Tensor()
-    assert True
+def test_shape_initialization():
+    """Test valid and invalid shape initialization."""
+    data = [1.0, 0.0, 2.0, 1.2]
+
+    valid_shapes = [4, (4,), (4, 1), (4, 1, 1), (2, 2), (2, 1, 2)]
+    for valid_shape in valid_shapes:
+        Tensor(data, valid_shape)
+
+    invalid_shapes = [3, (2, 3), (1, 1, 1), (-1,)]
+    for invalid_shape in invalid_shapes:
+        with pytest.raises(ValueError):
+            Tensor(data, invalid_shape)
+
+def test_adding_scalars():
+    """Test that you can add two scalar 'tensors'."""
+    a = Tensor(3.0, 1)
+    b = Tensor(1.2, 1)
+    assert a + b == Tensor(4.2, 1)
+
+def test_adding_1d_tensors():
+    """Test that you can add two 1D tensors."""
+    a = Tensor([-1.0, 2.0, -3.0], 3)
+    b = Tensor([3.0, 2.0, 1.0], 3)
+    assert a + b == Tensor([2.0, 4.0, -2.0], 3)
+
+def test_adding_multi_dimensional_tensors():
+    """Test that you can add two 2D+ tensors."""
+    a = Tensor([1.0, 2.0, 3.0, 4.0], (2, 2))
+    b = Tensor([3.0, 1.0, 2.0, 4.0], (2, 2))
+    assert a + b == Tensor([4.0, 3.0, 5.0, 8.0], (2, 2))
+
+def test_adding_different_shaped_tensors_fails():
+    """Test that you can't add two tensors with different shapes."""
+    # Can't add scalar and tensor (yet).
+    a = Tensor(3.0, 1)
+    b = Tensor([-1.0, 2.0, -3.0], 3)
+    with pytest.raises(ValueError):
+        a + b
+    
+    # Same number of elements but different shapes. 
+    c = Tensor([1.0, 2.0, 3.0, 4.0], (2, 2))
+    d = Tensor([3.0, 1.0, 2.0, 4.0], (4,))
+    with pytest.raises(ValueError):
+            c + d
